e404fc117bddffdb91bf647d8a4d81963e65472b
Sarah Naunton <sarah@naunton.org>
2024-04-01 16:37:32 +0100

    Set up routes for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<WelcomePage />} /> 
+        <Route path="sign-up" element={<SignUpPage />} /> 
+        <Route path="log-in" element={<LogInPage />} /> 
+        <Route path="home" element={<HomePage />} /> 
+        <Route path="mood-form" element={<MoodFormPage />} /> 
+        <Route path="mood-graph" element={<MoodGaphPage />} /> 
+        <Route path="journal" element={<JournalPage />} /> 
+        <Route path="*" element={<NotFoundPage />} /> 
+    </Routes>
+    </BrowserRouter>
   );
 }
 
