025d62810da56d3af97eaca87b65914f53e234f7
Sarah Naunton <sarah@naunton.org>
2024-04-15 16:34:52 +0100

    Change event to only happen on click rather than on page load, to avoid posting twice to database

--- src/components/Booster/BoosterDaily/BoosterDaily.js
+++ src/components/Booster/BoosterDaily/BoosterDaily.js
@@ -1,22 +1,21 @@
 import axios from "axios";
-import { useContext, useEffect, useState, useCallback } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../../contexts/ThemeContext";
 import star from "../../../assets/icons/star.png";
 import "./BoosterDaily.scss";
 
 export default function BoosterDaily({ boosterEntries }) {
   const { darkTheme } = useContext(ThemeContext);
-  const [recommendation, setRecommendation] = useState(null);
   const [errorMessage, setErrorMessage] = useState(false);
   const [text, setText] = useState("Click to Reveal");
   const [isAnimation, setIsAnimation] = useState(false);
 
-  const randomIndex = Math.floor(Math.random() * boosterEntries.length + 0);
-  const randomActivity = boosterEntries[randomIndex].activity;
+  const getRecommendation = async () => {
+    const authToken = localStorage.getItem("authToken");
+    const randomIndex = Math.floor(Math.random() * boosterEntries.length + 0);
+    const randomActivity = boosterEntries[randomIndex].activity;
+    let text = "";
 
-  const authToken = localStorage.getItem("authToken");
-
-  const getRecommendation = useCallback(async () => {
     try {
       const { data } = await axios.get(
         `${process.env.REACT_APP_API_BASE_URL}/recommendations`,
@@ -28,8 +27,7 @@ export default function BoosterDaily({ boosterEntries }) {
       );
 
       if (!data.length) {
-        setRecommendation(randomActivity);
-
+        text = randomActivity;
         await axios.post(
           `${process.env.REACT_APP_API_BASE_URL}/recommendations`,
           {
@@ -50,11 +48,11 @@ export default function BoosterDaily({ boosterEntries }) {
         const currentDate = new Date().toLocaleDateString("en-GB");
 
         if (updatedAt === currentDate) {
-          setRecommendation(data[0].recommendation);
+          text = data[0].recommendation;
         }
 
         if (updatedAt !== currentDate) {
-          setRecommendation(randomActivity);
+          text = randomActivity;
 
           await axios.put(
             `${process.env.REACT_APP_API_BASE_URL}/recommendations/${data[0].id}`,
@@ -70,67 +68,62 @@ export default function BoosterDaily({ boosterEntries }) {
         }
       }
       setErrorMessage(false);
+      setTimeout(() => {
+        setText(text);
+      }, 1500);
+      setIsAnimation(true);
     } catch (error) {
       setErrorMessage(error.response.data.error);
     }
-  }, [randomActivity, authToken]);
-
-  useEffect(() => {
-    getRecommendation();
-  }, [getRecommendation]);
+  };
 
   const handleClick = () => {
-    setTimeout(() => {
-      setText(recommendation);
-    }, 1500);
-    setIsAnimation(true);
+    getRecommendation();
   };
 
   return (
     <>
-      {recommendation && (
-        <div className={`inspiration ${darkTheme ? "inspiration--dark" : ""} `}>
-          <h2 className="inspiration__heading">Your daily inspiration</h2>
-          <p
-            className={`inspiration__text ${
-              darkTheme ? "inspiration__text--dark" : ""
+      <div className={`inspiration ${darkTheme ? "inspiration--dark" : ""} `}>
+        <h2 className="inspiration__heading">Your daily inspiration</h2>
+        <p
+          className={`inspiration__text ${
+            darkTheme ? "inspiration__text--dark" : ""
+          } `}
+        >
+          Discovering daily inspiration can be challenging, so why not leave it
+          to us?{" "}
+        </p>
+        <p
+          className={`inspiration__text ${
+            darkTheme ? "inspiration__text--dark" : ""
+          } `}
+        >
+          Give us a minute to think before clicking to reveal your activity. In
+          the meantime, take a moment to breathe.
+        </p>
+        <div className="inspiration__container" onClick={handleClick}>
+          <h3
+            className={`inspiration__activity ${
+              isAnimation ? "inspiration__activity--animation" : ""
             } `}
           >
-            Discovering daily inspiration can be challenging, so why not leave
-            it to us?{" "}
-          </p>
-          <p
-            className={`inspiration__text ${
-              darkTheme ? "inspiration__text--dark" : ""
-            } `}
-          >
-            Give us a minute to think before clicking to reveal your activity.
-            In the meantime, take a moment to breathe.
-          </p>
-          <div className="inspiration__container" onClick={handleClick}>
-            <h3
-              className={`inspiration__activity ${
-                isAnimation ? "inspiration__activity--animation" : ""
-              } `}
-            >
-              {text}
-            </h3>
-          </div>
-          <p
-            className={`inspiration__text ${
-              darkTheme ? "inspiration__text--dark" : ""
-            } `}
-          >
-            Try incorporating your selected activity into your daily schedule
-            for a refreshing boost.{" "}
-            <img
-              src={star}
-              alt="Star-Struck Emoji"
-              className="inspiration__icon"
-            />
-          </p>
+            {text}
+          </h3>
         </div>
-      )}
+        <p
+          className={`inspiration__text ${
+            darkTheme ? "inspiration__text--dark" : ""
+          } `}
+        >
+          Try incorporating your selected activity into your daily schedule for
+          a refreshing boost.{" "}
+          <img
+            src={star}
+            alt="Star-Struck Emoji"
+            className="inspiration__icon"
+          />
+        </p>
+      </div>
       {errorMessage && <p className="inspiration__error">{errorMessage}</p>}
     </>
   );
